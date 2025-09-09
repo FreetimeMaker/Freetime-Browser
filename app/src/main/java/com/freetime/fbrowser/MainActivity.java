fc8f023f03fa8628590c90f94f487cebbdbb6fe8
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-09-09 17:35:25 +0200

    Added the Browser

---
+++ app/src/main/java/com/freetime/fbrowser/MainActivity.java
@@ -0,0 +1,68 @@
+package com.freetime.fbrowser;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.freetime.browser.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager2 viewPager;
+    private List<Fragment> fragments = new ArrayList<>();
+    private FragmentStateAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        adapter = new FragmentStateAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        EditText urlInput = findViewById(R.id.urlInput);
+        Button goButton = findViewById(R.id.goButton);
+
+        goButton.setOnClickListener(v -> {
+            String url = urlInput.getText().toString();
+            if (!url.startsWith("http")) {
+                url = "https://" + url;
+            }
+            fragments.add(new WebFragment(url));
+            adapter.notifyItemInserted(fragments.size() - 1);
+            viewPager.setCurrentItem(fragments.size() - 1, true);
+        });
+    }
+
+    private class FragmentStateAdapter extends androidx.viewpager2.adapter.FragmentStateAdapter {
+        public FragmentStateAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragments.size();
+        }
+    }
+}
